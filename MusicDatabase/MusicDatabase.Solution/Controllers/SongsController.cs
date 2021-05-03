31c8dfc46929e2dccc2450591f2c3e084634aaf4
Chris Ramer <chrisramer.97@gmail.com>
2021-05-03 11:01:42 -0700

    Add create route & view for songs

--- MusicDatabase/MusicDatabase.Solution/Controllers/SongsController.cs
+++ MusicDatabase/MusicDatabase.Solution/Controllers/SongsController.cs
@@ -26,5 +26,30 @@ namespace MusicDatabase.Controllers
 			List<Song> model = _db.Songs.ToList();
 			return View(model);
 		}
+
+		public ActionResult Create()
+		{
+			ViewBag.ArtistId = new SelectList(_db.Artists, "ArtistId", "Name");
+			ViewBag.GenreId = new SelectList(_db.Genres, "GenreId", "Name");
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Create(Song song, int artistId, int genreId)
+		{
+			_db.Songs.Add(song);
+
+			if (artistId != 0)
+			{
+				_db.ArtistSong.Add(new ArtistSong() { ArtistId = artistId, SongId = song.SongId });
+			}
+			if (genreId != 0)
+			{
+				_db.GenreSong.Add(new GenreSong() { GenreId = genreId, SongId = song.SongId });
+			}
+
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+		}
 	}
 }
